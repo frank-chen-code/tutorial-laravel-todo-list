cd447a84fa6be97187e8d53e0261ff3ff16d8783
frank-chen-code <frank.chen@wolves.com.tw>
2020-11-03 00:02:23 +0800

    fix-route

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,9 +7,11 @@
     <div class="row">
         <div class="col-sm-12">
             <h1>Create Task</h1>
-        </div>
-    </div>
 
-    @component('components.taskForm')
-    @endcomponent
+            {!! Form::open(['route' => 'task.store', 'method' => 'POST']) !!}
+                @component('components.taskForm')
+                @endcomponent
+            {!! Form::close() !!}
+         </div>
+    </div>
 @endsection
