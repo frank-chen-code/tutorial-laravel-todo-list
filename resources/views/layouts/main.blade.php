cd3b04033eca0f368ef8405f2ebcdf6973a0a44e
frank-chen-code <frank.chen@wolves.com.tw>
2020-11-02 21:46:20 +0800

    indexview

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <title>ToDo List APP - @yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <style>
+        .pagination{
+            justify-content: center;
+        }
+    </style>
 </head>
 <body>
     <div class="container mt-3">
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.main')
+
+@section('title', 'Tasks Home')
+
+@section('content')
+    @foreach($tasks as $task)
+        <div class="row">
+            <div class="col-sm-12">
+                <h3>
+                    {{ $task->name }}
+                    <small>{{ $task->created_at }}</small>
+                </h3>
+                <hr>
+                <p>{{ $task->description }}</p>
+                <h4>{{ $task->due_date }}</h4>
+
+            </div>
+            <hr>
+
+        </div>
+    @endforeach
+    <div class="row justify-content-center">
+        <div class="col-sm-6 text-center">
+            {{ $tasks->links() }}
+        </div>
+    </div>
+@endsection
